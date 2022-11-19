caff7ae3161e66e04bc8ebc34f903fd3596bd4ab
Mateusz Domański <mateusz.domanski@volt.io>
2022-11-19 14:10:41 +0100

    fix: not needed imports from initial config

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,6 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { default as bemCssModules } from "bem-css-modules";
 
-import { Content } from "./components/Content/Content";
-import { Header } from "./components/Header/Header";
-
 import { store } from "./stores/store";
 
 import "./App.css";
