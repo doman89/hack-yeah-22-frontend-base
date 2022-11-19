1417f2a17340018660d87ea02831d11a66508b30
Mateusz Domański <mateusz.domanski@volt.io>
2022-11-19 22:09:06 +0100

    fix: map is visible

--- src/modules/map/pages/MapPage.tsx
+++ src/modules/map/pages/MapPage.tsx
@@ -18,11 +18,12 @@ export default function MapPage() {
   }
 
   return (
-    <Box sx={{ maxWidth: "100%", maxHeight: "100%" }}>
+    <Box sx={{ maxWidth: "100%", width: "100%", maxHeight: "100%", height: "100vh" }}>
       <MapContainer
+        style={{ height: "100vh" }}
         center={coords ? [coords?.latitude, coords?.longitude] : [0, 0]}
         zoom={13}
-        scrollWheelZoom={false}
+        scrollWheelZoom
       >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
